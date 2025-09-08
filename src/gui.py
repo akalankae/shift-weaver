94dc288557b91e209b569808dc61ad4992133deb
Akalanka Edirisinghe <akalankae@gmail.com>
2025-09-09 00:28:26 +1000

    feat(GUI): create the main graphical windows
    - Application has 3 graphical windows: LoginWindow, UploadWindow,
      NameSelectWindow
    - Appearance ony, none of these are still functioning

---
+++ src/gui.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# gui.py
+# Graphical User Interface for roster synchronizer program.
+
+import sys
+from typing import override, final
+import random
+from pathlib import Path
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont, QShowEvent
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
+    QSizePolicy,
+    QButtonGroup,
+    QFrame,
+    QComboBox,
+)
+
+
+@final
+class LoginWindow(QWidget):
+    """
+    User enters his/her icloud account credentials at Login Window.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Login Window")
+        self.setMinimumWidth(360)
+
+        heading = QLabel("iCloud User Credentials", self)
+        heading.setFont(QFont("Times", 16, 500, True))
+        username_lbl = QLabel("Username:", self)
+        username_lbl.setAlignment(Qt.AlignmentFlag.AlignRight)
+        username_entry = QLineEdit(self)
+        password_lbl = QLabel("Password:", self)
+        password_lbl.setAlignment(Qt.AlignmentFlag.AlignRight)
+        password_entry = QLineEdit(self)
+        save_creds_cb = QCheckBox("Save Credentials", self)
+        enter_btn = QPushButton("Enter", self)
+        quit_btn = QPushButton("Quit", self)
+
+        # visually seperate action buttons from form
+        separator = QFrame(self)
+        separator.setFrameShape(QFrame.Shape.NoFrame)
+        separator.setFrameShadow(QFrame.Shadow.Sunken)
+        separator.setLineWidth(1)
+        separator.setMidLineWidth(0)
+        separator.setFixedHeight(20)
+
+        root_layout = QVBoxLayout(self)
+        cred_field_layout = QGridLayout()
+        cred_field_layout.addWidget(username_lbl, 0, 0)
+        cred_field_layout.addWidget(username_entry, 0, 1)
+        cred_field_layout.addWidget(password_lbl, 1, 0)
+        cred_field_layout.addWidget(password_entry, 1, 1)
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(quit_btn)
+        button_layout.addWidget(enter_btn)
+
+        root_layout.addWidget(heading, 0, Qt.AlignmentFlag.AlignHCenter)
+        root_layout.addLayout(cred_field_layout, 0)
+        root_layout.addWidget(save_creds_cb, 0, Qt.AlignmentFlag.AlignHCenter)
+        root_layout.addWidget(separator)
+        root_layout.addLayout(button_layout, 0)
+
+        self.setLayout(root_layout)
+
+
+@final
+class UploadWindow(QWidget):
+    """
+    User selects type of roster and roster file to upload.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Upload Roster")
+        self.setFixedWidth(360)
+
+        heading_lbl_1 = QLabel("Select Roster Type", self)
+        heading_lbl_1.setFont(QFont("Times", 14, 250, True))
+        term_roster_cbox = QCheckBox("Term Roster", self)
+        week_roster_cbox = QCheckBox("Week Roster", self)
+
+        roster_type = QButtonGroup(self)
+        roster_type.setExclusive(True)
+        roster_type.addButton(term_roster_cbox, 1)
+        roster_type.addButton(week_roster_cbox, 2)
+
+        heading_lbl_2 = QLabel("Select Roster File", self)
+        heading_lbl_2.setFont(QFont("Times", 14, 250, True))
+        self.upload_btn = QPushButton("Upload", self)
+        self.upload_btn.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
+        self.enter_btn = QPushButton("Enter", self)
+        self.quit_btn = QPushButton("Quit", self)
+
+        # visually seperate action buttons from form
+        separator = QFrame(self)
+        separator.setFrameShape(QFrame.Shape.NoFrame)
+        separator.setFrameShadow(QFrame.Shadow.Sunken)
+        separator.setLineWidth(1)
+        separator.setMidLineWidth(0)
+        separator.setFixedHeight(20)
+
+        selection_layout = QGridLayout()
+        selection_layout.setHorizontalSpacing(40)
+        selection_layout.addWidget(heading_lbl_1, 0, 0, 1, 1)
+        selection_layout.addWidget(heading_lbl_2, 0, 1, 1, 1)
+        selection_layout.addWidget(
+            term_roster_cbox, 1, 0, 1, 1, Qt.AlignmentFlag.AlignHCenter
+        )
+        selection_layout.addWidget(
+            week_roster_cbox, 2, 0, 1, 1, Qt.AlignmentFlag.AlignHCenter
+        )
+        selection_layout.addWidget(self.upload_btn, 1, 1, 2, 1)
+
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(self.quit_btn, 0, Qt.AlignmentFlag.AlignRight)
+        btn_layout.addStretch()
+        btn_layout.addWidget(self.enter_btn, 0, Qt.AlignmentFlag.AlignRight)
+
+        root_layout = QVBoxLayout(self)
+        root_layout.addLayout(selection_layout, 0)
+        root_layout.addWidget(separator)
+        root_layout.addLayout(btn_layout, 0)
+
+    @override
+    def showEvent(self, a0: QShowEvent | None):
+        """
+        Manually resize "Enter" and "Quit" buttons, so that they don't end up too
+        small compared to "Upload" button.
+        """
+        upload_btn_height = self.upload_btn.height()
+        upload_btn_width = self.upload_btn.width()
+        self.enter_btn.setFixedSize(upload_btn_width, upload_btn_height // 2)
+        self.quit_btn.setFixedSize(upload_btn_width, upload_btn_height // 2)
+        super().showEvent(a0)
+
+
+@final
+class NameSelectWindow(QWidget):
+    """
+    User selects his/her name in the roster.
+    """
+
+    def __init__(self, name_list: list[str]):
+        super().__init__()
+        self.setWindowTitle("Select Your Name")
+        self.setFixedWidth(360)
+
+        heading_lbl = QLabel("Select Your Name", self)
+        heading_lbl.setFont(QFont("Times", 16, 500, True))
+
+        back_btn = QPushButton("Go Back", self)
+        enter_btn = QPushButton("Enter", self)
+        enter_btn.setMinimumWidth(160)
+        quit_btn = QPushButton("Quit", self)
+        quit_btn.setMinimumWidth(160)
+        names_cbox = QComboBox(self)
+        names_cbox.addItems(name_list)
+        names_cbox.setMaxVisibleItems(12)
+        save_creds_cb = QCheckBox("Remember Me", self)
+
+        # visually seperate action buttons from form
+        separator = QFrame(self)
+        separator.setFrameShape(QFrame.Shape.NoFrame)
+        separator.setFrameShadow(QFrame.Shadow.Sunken)
+        separator.setLineWidth(1)
+        separator.setMidLineWidth(0)
+        separator.setFixedHeight(20)
+
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(quit_btn)
+        btn_layout.addStretch()
+        btn_layout.addWidget(enter_btn)
+
+        root_layout = QVBoxLayout(self)
+        root_layout.addWidget(back_btn, 0, Qt.AlignmentFlag.AlignLeft)
+        root_layout.addWidget(heading_lbl, 0, Qt.AlignmentFlag.AlignHCenter)
+        root_layout.addWidget(names_cbox, 0, Qt.AlignmentFlag.AlignHCenter)
+        root_layout.addWidget(save_creds_cb, 0, Qt.AlignmentFlag.AlignHCenter)
+        root_layout.addWidget(separator)
+        root_layout.addLayout(btn_layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    win_1 = LoginWindow()
+    win_1.show()
+    win_2 = UploadWindow()
+    win_2.show()
+
+    FILE = "data/names.txt"  # file with list of random full names
+    NUM_RANDOM_NAMES = 50  # number of names to show in ComboBox
+    try:
+        data = Path(FILE).read_text("utf-8")
+        name_list = [
+            s.title() for s in random.sample(data.splitlines(), NUM_RANDOM_NAMES)
+        ]
+    except FileNotFoundError:
+        sys.stderr.write(f"Could not find file: {FILE}")
+        name_list = [
+            "Jack Napier",
+            "Jill Taylor",
+            "Robert Pattingson",
+            "Anne Hathaway",
+            "Patrick Jane",
+        ]
+    win_3 = NameSelectWindow(name_list)
+    win_3.show()
+
+    sys.exit(app.exec())
