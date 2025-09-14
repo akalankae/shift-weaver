58afb9b4e302a7154e777fcd84fb3e6559fd56a1
Akalanka Edirisinghe <akalankae@gmail.com>
2025-09-14 13:23:31 +1000

    wip(excel_parser): utilize excel parsing by main program

--- src/main.py
+++ src/main.py
@@ -12,6 +12,9 @@ import sys
 from PyQt6.QtWidgets import QApplication
 
 from gui import LoginWindow, UploadWindow
+from excel import find_date_row, find_name_column
+
+from openpyxl import load_workbook
 
 userdata: dict[str, str] = dict()  # username, password, roster_type, roster_path
 
@@ -29,6 +32,23 @@ upload_win.show()
 app.exec()
 
 # Get a list of names in the roster
+roster_type = userdata.get("roster_type")
+if roster_type == "term":
+    roster_path = userdata.get("roster_path")
+    if roster_path:
+        print(f"Opening roster: {roster_path}")
+        wb = load_workbook(roster_path)
+        if not wb.active:
+            sys.stderr.write("No Active Worksheet\n")
+            sys.exit(1)
+        dates_row = find_date_row(wb.active)
+        names_col = find_name_column(wb.active)
+        if not names_col:
+            sys.stderr.write("Cannot find names column\n")
+            sys.exit(1)
+        possible_names:dict[str, int] = {
+                cell.value:cell.row for cell in wb.active[names_col] if cell.data_type == "s"
+        }
 
 
 # Contact CalDAV server with username/password to get a list of shifts for the
